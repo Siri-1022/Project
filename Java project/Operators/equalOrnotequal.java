93039f180190fa00d6d377fd7c78a590846f783e
Siri-1022 <sireesha.mallela23@gmail.com>
2025-11-16 17:24:37 +0530

    equalOrnotequal

---
+++ Java project/Operators/equalOrnotequal.java
@@ -0,0 +1,13 @@
+public class equalOrnotequal {
+    public static void main(String args[]){
+        int val=100;
+        float num=100;
+        // == checks the value of variable regardless of datatype
+        if(val==num){
+            System.out.println("Both values are equal");
+        }
+        else{
+            System.out.println("Not equal");
+        }
+    }
+}
