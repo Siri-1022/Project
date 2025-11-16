76947cd34e9425fbf59b1e01a26f7eea9dcd1f27
Siri-1022 <sireesha.mallela23@gmail.com>
2025-11-16 16:44:29 +0530

    EvenorOdd

---
+++ Java project/Loops/EvenorOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenorOdd {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int number = sc.nextInt();
+        int rem=number%2;
+        switch(rem){
+            case 0:
+            System.out.println("Even");
+            break;
+            case 1:
+            System.out.println("Odd");
+            break;
+            default:
+            System.out.println("Error");
+        }
+    }
+}
