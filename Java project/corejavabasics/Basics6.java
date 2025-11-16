7e25c597925d747c33a1ebff9f8acf1731beaea5
Siri-1022 <sireesha.mallela23@gmail.com>
2025-11-16 16:32:21 +0530

    Basics6

---
+++ Java project/corejavabasics/Basics6.java
@@ -0,0 +1,11 @@
+public class Basics6 {
+    public static String myName(String name)
+    {
+        return name;
+    }
+    public static void main(String args[])
+    {
+        String full_name = "Mallela Sireesha";
+        System.out.println(myName(full_name));
+    }
+}
