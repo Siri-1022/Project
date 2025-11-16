ec6d1bc72a2d69dea6ca6c6e1e578cfbce41b437
Siri-1022 <sireesha.mallela23@gmail.com>
2025-11-16 16:49:27 +0530

    dowhile

---
+++ Java project/Loops/dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void numbers(){
+        int i=1;
+        do { 
+            System.out.print(i+" ");
+            i++;
+        } while (i<=10);
+        
+    }
+    public static void main(String args[]){
+        numbers();
+    }
+}
