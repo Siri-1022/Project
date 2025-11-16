cf9d8125048f14e805f453b9cf5554546bcf7d9c
Siri-1022 <sireesha.mallela23@gmail.com>
2025-11-16 16:57:54 +0530

    forloop

---
+++ Java project/Loops/forloop.java
@@ -0,0 +1,8 @@
+public class forloop {
+    public static void main(String args[]){
+        String str= "Bright IT Career";
+        for(int i=1;i<=10;i++){
+            System.out.println(str);
+        }
+    }
+}
